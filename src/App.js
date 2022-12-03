3bf965a22b74335fa5d05b280495547468007860
Albert Makai Ngabo <ngabo@Ngabos-MacBook-Pro.local>
2022-12-03 11:53:42 +0300

    create pages and router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
     <>
-      <h1 className='text-2xl'>Hello world!</h1>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Profile" element={<Profile/>}/>
+        <Route path="/Sign-in" element={<SignIn/>}/>
+        <Route path="/Sign-up" element={<SignUp/>}/>
+        <Route path="/Forgot-Password" element={<ForgotPassword/>}/>
+      </Routes>
+     </Router>
     </>
   );
 }
